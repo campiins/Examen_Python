3d04ae4a44a2e81f7f573f6b740ee26f52106f00
Marc <marcamp1@epsg.upv.es>
2022-03-07 13:03:08 +0100

    checkpoint 6

--- funciones.py
+++ funciones.py
@@ -25,16 +25,14 @@ def add_client(clients_list,nif,name,address,phone,email):
       phone
       email
     """
-    # (1) anteriormente se cogía clients_list como una lista en vez de un diccionario y se intentaba insertar los datos
-    # en la posición igual al nif de la persona, lo cual no tiene mucho sentido
-    # (2) he usado el método update para insertar o actualizar en un diccionario nueva información
-    clients_list.update({
-        nif: {'name': name,
+    # (1) se intentaba meter como valor de la clave nif de clients_list otro diccionario con todos los datos
+    # (2) he quitado el diccionario que se creaba dentro
+    clients_list[nif] = {
+              'name': name,
               'address': address,
               'phone': phone,
               'email': email
-        }
-    })
+    }
 
 def repartir_cartas(cartas_iniciales,repeticiones):
     """Dada una baraja de cartas iniciales y un número de repeticiones, esta función selecciona 5 cartas aleatorias de esta baraja y las mete en un diccionario llamado combinaciones. El proceso se repite tantas veces como repeticiones se indiquen.
@@ -44,18 +42,13 @@ def repartir_cartas(cartas_iniciales,repeticiones):
     Returns:
       combinaciones: ej. {'repeticion1': ['contable', 'alguacil', 'asesino', 'cardenal', 'obispo']}
     """    
-    # (1) el error aparece a la hora de añadir cartas al diccionario
+    # (1) da error al intentar hacer más de 2 repeticiones
     combinaciones={}
     for i in range(1,repeticiones+1):
-        cartas_aleatorias = cartas_iniciales 
-        combinaciones.update("repeticion"+str(i) : [])
+        cartas_aleatorias = cartas_iniciales
+        combinaciones["repeticion"+str(i)]=[]
         for j in range(0,5):
             carta=random.choice(cartas_aleatorias)
             combinaciones["repeticion"+str(i)].append(carta)
             cartas_aleatorias.remove(carta)
-
     return combinaciones
-
-cartas_iniciales = ["reina","guardia","asesino","obispo","alguacil","bufon","contable","adulador","baronesa","cardenal"]
-combinaciones = repartir_cartas(cartas_iniciales,3)
-print(combinaciones)
